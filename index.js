c73a32b046618b723378e1bb11b0f143a77e3730
Raikas <roni.aikas@gmail.com>
2021-03-07 17:52:53 +0200

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const updater = require('gh-updater');
+const version = require('./package.json').version;
+// to check version
+updater.check_version('raikasdev/auto-update-test-main', version_data => {
+    console.log(JSON.stringify(version_data))
+    if (version_data.has_new_version) {
+        // to update
+        updater.update('kanasimi/gh-updater-master', '.');
+    }
+});
+ 
